3e2e616cd09af1bcec0c4168c6ed0cc6ff3cff9d
owaisrafiq05 <owaisrafiq05@gmail.com>
2024-06-01 01:06:37 +0500

    frontend updated

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors"
-require('dotenv').config()
+// require('dotenv').config()
 import route from "./routes/index.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,9 +10,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({ origin: "*" }))
 
-// const uri = "mongodb+srv://admin:admin@cluster0.oplpgp2.mongodb.net/toDoList"
+const uri = "mongodb+srv://admin:admin@cluster0.oplpgp2.mongodb.net/toDoList"
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(uri)
     .then(() => console.log("mongodb connected!"))
     .catch((error) => console.log("err mongodb", error.message))
 
--- to-do-list-frontend/src/App.jsx
+++ to-do-list-frontend/src/App.jsx
@@ -1,13 +1,13 @@
-import React, { useState , useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { base_url } from '../config/index.js'
 import axios from "axios";
 
 const App = () => {
-  const [input,setInput] = useState("");
-  const [post,setPost] = useState([]);
+  const [input, setInput] = useState("");
+  const [post, setPost] = useState([]);
 
   const addTodo = async () => {
-    try{
+    try {
       console.log("addTodoHandler");
       const obj = {
         value: input,
@@ -16,14 +16,14 @@ const App = () => {
       const create = await axios.post(`${base_url}/createpost`, obj);
       console.log("create", create);
     }
-    catch(error){
-      console.log("Error",error.message);
+    catch (error) {
+      console.log("Error", error.message);
     }
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchData();
-  },[]);
+  }, []);
 
   const fetchData = async () => {
     try {
@@ -75,10 +75,43 @@ const App = () => {
 
       {post?.map((post) => {
         return (
-          <div key={post._id}>
-            <p> {post.value}</p>
-            <button onClick={() => editHandler(post._id)}>EDIT</button>
-            <button onClick={() => deleteHandler(post._id)}>DELETE</button>
+          // <div key={post._id}>
+          //   <p> {post.value}</p>
+          //   <button onClick={() => editHandler(post._id)}>EDIT</button>
+          //   <button onClick={() => deleteHandler(post._id)}>DELETE</button>
+          // </div>
+          <div className='flex justify-evenly items-center'>
+              <div key={post._id}>
+                <article
+                  class="w-72 bg-gray-700 shadow p-4 space-y-2 rounded-md hover:-translate-y-2 duration-300"
+                >
+                  <svg
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    height="24"
+                    width="24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                    class="w-10 h-10 text-gray-300 bg-gray-600 rounded-full p-1"
+                  >
+                    <path
+                      d="m8.032 12 1.984 1.984 4.96-4.96m4.55 5.272.893-.893a1.984 1.984 0 0 0 0-2.806l-.893-.893a1.984 1.984 0 0 1-.581-1.403V7.04a1.984 1.984 0 0 0-1.984-1.984h-1.262a1.983 1.983 0 0 1-1.403-.581l-.893-.893a1.984 1.984 0 0 0-2.806 0l-.893.893a1.984 1.984 0 0 1-1.403.581H7.04A1.984 1.984 0 0 0 5.055 7.04v1.262c0 .527-.209 1.031-.581 1.403l-.893.893a1.984 1.984 0 0 0 0 2.806l.893.893c.372.372.581.876.581 1.403v1.262a1.984 1.984 0 0 0 1.984 1.984h1.262c.527 0 1.031.209 1.403.581l.893.893a1.984 1.984 0 0 0 2.806 0l.893-.893a1.985 1.985 0 0 1 1.403-.581h1.262a1.984 1.984 0 0 0 1.984-1.984V15.7c0-.527.209-1.031.581-1.403Z"
+                      stroke-width="2"
+                      stroke-linejoin="round"
+                      stroke-linecap="round"
+                      stroke="currentColor"
+                    ></path>
+                  </svg>
+                  <p class="text-sm w-full text-gray-400">
+                  {post.value}
+                  </p>
+                  <br/>
+                  <div className='flex justify-between align-center'>
+                    <button onClick={() => editHandler(post._id)}>EDIT</button>
+                    <button onClick={() => deleteHandler(post._id)}>DELETE</button>
+                  </div>
+                </article>
+              </div>
           </div>
         );
       })}
