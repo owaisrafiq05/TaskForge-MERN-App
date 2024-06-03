73599e3d71a20e2785379bfc5235590e89e57eac
owaisrafiq05 <owaisrafiq05@gmail.com>
2024-06-04 00:52:38 +0500

    ui updated

--- to-do-list-frontend/src/App.jsx
+++ to-do-list-frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { base_url } from '../config/index.js'
+import './App.css';
 import axios from "axios";
+import { StickyNavbar } from './components/navbar.jsx';
 
 const App = () => {
   const [input, setInput] = useState("");
@@ -15,6 +17,7 @@ const App = () => {
       };
       const create = await axios.post(`${base_url}/createpost`, obj);
       console.log("create", create);
+      location.reload();
     }
     catch (error) {
       console.log("Error", error.message);
@@ -65,56 +68,60 @@ const App = () => {
   };
 
   return (
-    <div>
-      <input
-        onChange={(e) => setInput(e.target.value)}
-        type="text"
-        placeholder="enter value"
-      />
-      <button onClick={addTodo}>ADD TODO</button>
+    <div className="p-0 m-0 w-screen h-full bg-black">
+      <StickyNavbar />
+      <div>
+        <div className='w-screen flex justify-center items-center flex-col gap-y-9'>
+          {/* <input
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            placeholder="enter value"
+            className=''
+          /> */}
+          <input
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            placeholder="Write Your To Do Task..."
+            name="text"
+            class="input"></input>
+          <button onClick={addTodo}>ADD TO DO</button>
+        </div>
 
-      {post?.map((post) => {
-        return (
-          // <div key={post._id}>
-          //   <p> {post.value}</p>
-          //   <button onClick={() => editHandler(post._id)}>EDIT</button>
-          //   <button onClick={() => deleteHandler(post._id)}>DELETE</button>
-          // </div>
-          <div className='flex justify-evenly items-center'>
-              <div key={post._id}>
-                <article
-                  class="w-72 bg-gray-700 shadow p-4 space-y-2 rounded-md hover:-translate-y-2 duration-300"
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4 w-screen pt-8'>
+          {post.map((post) => (
+            <div key={post._id}>
+              <article className="bg-gray-700 shadow p-4 space-y-2 rounded-md hover:-translate-y-2 duration-300">
+                <svg
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  height="24"
+                  width="24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  className="w-10 h-10 text-gray-300 bg-gray-600 rounded-full p-1"
                 >
-                  <svg
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    height="24"
-                    width="24"
-                    xmlns="http://www.w3.org/2000/svg"
-                    aria-hidden="true"
-                    class="w-10 h-10 text-gray-300 bg-gray-600 rounded-full p-1"
-                  >
-                    <path
-                      d="m8.032 12 1.984 1.984 4.96-4.96m4.55 5.272.893-.893a1.984 1.984 0 0 0 0-2.806l-.893-.893a1.984 1.984 0 0 1-.581-1.403V7.04a1.984 1.984 0 0 0-1.984-1.984h-1.262a1.983 1.983 0 0 1-1.403-.581l-.893-.893a1.984 1.984 0 0 0-2.806 0l-.893.893a1.984 1.984 0 0 1-1.403.581H7.04A1.984 1.984 0 0 0 5.055 7.04v1.262c0 .527-.209 1.031-.581 1.403l-.893.893a1.984 1.984 0 0 0 0 2.806l.893.893c.372.372.581.876.581 1.403v1.262a1.984 1.984 0 0 0 1.984 1.984h1.262c.527 0 1.031.209 1.403.581l.893.893a1.984 1.984 0 0 0 2.806 0l.893-.893a1.985 1.985 0 0 1 1.403-.581h1.262a1.984 1.984 0 0 0 1.984-1.984V15.7c0-.527.209-1.031.581-1.403Z"
-                      stroke-width="2"
-                      stroke-linejoin="round"
-                      stroke-linecap="round"
-                      stroke="currentColor"
-                    ></path>
-                  </svg>
-                  <p class="text-sm w-full text-gray-400">
+                  <path
+                    d="m8.032 12 1.984 1.984 4.96-4.96m4.55 5.272.893-.893a1.984 1.984 0 0 0 0-2.806l-.893-.893a1.984 1.984 0 0 1-.581-1.403V7.04a1.984 1.984 0 0 0-1.984-1.984h-1.262a1.983 1.983 0 0 1-1.403-.581l-.893-.893a1.984 1.984 0 0 0-2.806 0l-.893.893a1.984 1.984 0 0 1-1.403.581H7.04A1.984 1.984 0 0 0 5.055 7.04v1.262c0 .527-.209 1.031-.581 1.403l-.893.893a1.984 1.984 0 0 0 0 2.806l.893.893c.372.372.581.876.581 1.403v1.262a1.984 1.984 0 0 0 1.984 1.984h1.262c.527 0 1.031.209 1.403.581l.893.893a1.984 1.984 0 0 0 2.806 0l.893-.893a1.985 1.985 0 0 1 1.403-.581h1.262a1.984 1.984 0 0 0 1.984-1.984V15.7c0-.527.209-1.031.581-1.403Z"
+                    stroke-width="2"
+                    stroke-linejoin="round"
+                    stroke-linecap="round"
+                    stroke="currentColor"
+                  ></path>
+                </svg>
+                <p className="text-sm w-full text-gray-400">
                   {post.value}
-                  </p>
-                  <br/>
-                  <div className='flex justify-between align-center'>
-                    <button onClick={() => editHandler(post._id)}>EDIT</button>
-                    <button onClick={() => deleteHandler(post._id)}>DELETE</button>
-                  </div>
-                </article>
-              </div>
-          </div>
-        );
-      })}
+                </p>
+                <br />
+                <div className='flex justify-between align-center'>
+                  <button onClick={() => editHandler(post._id)}>EDIT</button>
+                  <button onClick={() => deleteHandler(post._id)}>DELETE</button>
+                </div>
+              </article>
+            </div>
+          ))}
+        </div>
+
+      </div>
     </div>
   )
 }
---
+++ to-do-list-frontend/src/components/navbar.jsx
@@ -0,0 +1,107 @@
+import React from "react";
+import {
+  Navbar,
+  MobileNav,
+  Typography,
+  Button,
+  IconButton,
+  Card,
+} from "@material-tailwind/react";
+ 
+export function StickyNavbar() {
+  const [openNav, setOpenNav] = React.useState(false);
+ 
+  React.useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setOpenNav(false),
+    );
+  }, []);
+ 
+  const navList = (
+    <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center text-red-500">
+          Home
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center text-red-500">
+          About 
+        </a>
+      </Typography>
+    </ul>
+  );
+ 
+  return (
+    <div className="pt-0 mt-0 max-h-[768px] w-[calc(100%+48px)] w-screen ">
+      <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none bg-black px-4 py-2 lg:px-8 lg:py-4 border-none">
+        <div className="flex items-center justify-between text-red-500">
+          <Typography
+            as="a"
+            href="#"
+            className="text-3xl mr-4 cursor-pointer font-extrabold py-1.5 "
+          >
+            To Do List
+          </Typography>
+          <div className="flex items-center gap-4">
+            <div className="mr-4 hidden lg:block">{navList}</div>
+            <IconButton
+              variant="text"
+              className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+              ripple={false}
+              onClick={() => setOpenNav(!openNav)}
+            >
+              {openNav ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  className="h-6 w-6"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </IconButton>
+          </div>
+        </div>
+        <MobileNav open={openNav}>
+          {navList}
+        </MobileNav>
+      </Navbar>
+      <div className="mx-auto max-w-screen-md py-12">
+        
+      </div>
+    </div>
+  );
+}
